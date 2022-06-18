f5dcc239c571c301fc07814a445a93a56f73afd4
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-18 14:45:46 +0430

    [FEATURE] adding routes and main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import styles from "./app.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.card}>
+      <h1>you can add user or product</h1>
+      <p>this app is just a simple app that renders forms base on json file </p>
+      <p>
+        you can{" "}
+        <Link className={styles.link} to="/addusers">
+          add user
+        </Link>{" "}
+        or{" "}
+        <Link className={styles.link} to="/addproduct">
+          add product
+        </Link>
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Layout from './components/Layout/Layout'
+import AddFormPage from './pages/AddFormPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="addusers" element={<AddFormPage/>} />
+        <Route path="addproduct" element={<AddFormPage/>} />
+      </Routes>
+    </Layout>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
